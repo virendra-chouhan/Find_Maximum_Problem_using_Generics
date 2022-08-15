d5e24d399cebc7771a844ecc8caef7ab533998a2
virendra <virendrachouhan1494@gmail.com>
2022-08-15 13:28:32 +0530

    UC2

--- Find_Maximum_Problem_Using_Genrick/Program.cs
+++ Find_Maximum_Problem_Using_Genrick/Program.cs
@@ -8,11 +8,11 @@ namespace Find_Maximum_Problem_Using_Genrick
         {
             Console.WriteLine("Hello World!");
 
-            static void Main(string[] args)
-            {
+            
+            
                 Maximum_Number obj1 = new Maximum_Number();
-                obj1.Find_Max_Num(25, 56, 11);
-            }
+                obj1.Find_Max_Num(25, 56, 88);
+            
         }
     }
 }
