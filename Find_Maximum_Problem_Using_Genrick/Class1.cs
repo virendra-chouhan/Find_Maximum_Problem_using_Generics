0e463957fc9f0a0db4c2fe75586b25dd4d529c50
virendra <virendrachouhan1494@gmail.com>
2022-08-15 13:55:09 +0530

    UC4

--- Find_Maximum_Problem_Using_Genrick/Class1.cs
+++ Find_Maximum_Problem_Using_Genrick/Class1.cs
@@ -4,11 +4,48 @@ using System.Text;
 
 namespace Find_Maximum_Problem_Using_Genrick
 {
-    class Printer
+    public class MaxNumbers
     {
-        public void Print<T>(T data)
+        public static void maxNumbers()
         {
-            Console.WriteLine(data);
+            int num1 = 554;
+            int num2 = 222;
+            int num3 = 822;
+            int num4 = 220;
+
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) > 0 ||
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0 && num1.CompareTo(num4) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) >= 0)
+
+            {
+                Console.WriteLine("num 1 is greater " + num1);
+            }
+
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) > 0 ||
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0 && num2.CompareTo(num4) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) >= 0)
+            {
+                Console.WriteLine("num 2 is greater " + num2);
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) > 0 ||
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0 && num3.CompareTo(num4) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) >= 0)
+            {
+                Console.WriteLine("num 3 is greater " + num3);
+            }
+
+            if (num4.CompareTo(num1) > 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) > 0 ||
+               num4.CompareTo(num1) >= 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) > 0 ||
+                num4.CompareTo(num1) > 0 && num4.CompareTo(num2) >= 0 && num4.CompareTo(num3) > 0 ||
+                num4.CompareTo(num1) > 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("num 4 is greater " + num4);
+            }
+            Console.ReadKey();
         }
     }
 }
--- Find_Maximum_Problem_Using_Genrick/Program.cs
+++ Find_Maximum_Problem_Using_Genrick/Program.cs
@@ -2,22 +2,15 @@
 
 namespace Find_Maximum_Problem_Using_Genrick
 {
-    class Generic<T>
-    {
-        public Generic(T msg)
-        {
-            Console.WriteLine(msg);
-            Console.ReadLine();
-        }
+    
 
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Generic<string> gen = new Generic<string>("This is generic class");
-            Generic<int> genI = new Generic<int>(101);
-            Generic<char> getCh = new Generic<char>('I');
+            MaxNumbers.maxNumbers();
+            Console.ReadKey();
         }
     }
 }
