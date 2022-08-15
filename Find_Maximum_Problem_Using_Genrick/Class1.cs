3d76ee8a22f1ca36388fa03360db05c51a4de70b
virendra <virendrachouhan1494@gmail.com>
2022-08-15 13:44:41 +0530

    UC3.2

--- Find_Maximum_Problem_Using_Genrick/Class1.cs
+++ Find_Maximum_Problem_Using_Genrick/Class1.cs
@@ -4,23 +4,11 @@ using System.Text;
 
 namespace Find_Maximum_Problem_Using_Genrick
 {
-    class Maximum_Number
+    class Printer
     {
-        public void Find_Max_Num(string First, string Secound, string Third)
+        public void Print<T>(T data)
         {
-            if (First.CompareTo(Secound) > 0 && First.CompareTo(Third) > 0 || First.CompareTo(Secound) >= 0 && First.CompareTo(Third) > 0 || First.CompareTo(Secound) > 0 && First.CompareTo(Third) >= 0)
-            {
-                Console.WriteLine(First + " is greter than " + Secound + " " + Third);
-            }
-            else if (Secound.CompareTo(First) > 0 && Secound.CompareTo(Third) > 0 || Secound.CompareTo(First) >= 0 && Secound.CompareTo(Third) > 0 || Secound.CompareTo(First) > 0 && Secound.CompareTo(Third) >= 0)
-            {
-                Console.WriteLine(Secound + " is greter than " + First + " " + Third);
-            }
-            else if (Third.CompareTo(First) > 0 && Third.CompareTo(Secound) > 0 || Third.CompareTo(Secound) >= 0 && Third.CompareTo(First) > 0 || Third.CompareTo(Secound) > 0 && Third.CompareTo(Secound) >= 0)
-            {
-                Console.WriteLine(Third + " is greter than " + Secound + " " + First);
-            }
-            Console.ReadKey();
+            Console.WriteLine(data);
         }
     }
 }
--- Find_Maximum_Problem_Using_Genrick/Program.cs
+++ Find_Maximum_Problem_Using_Genrick/Program.cs
@@ -2,17 +2,22 @@
 
 namespace Find_Maximum_Problem_Using_Genrick
 {
+    class Generic<T>
+    {
+        public Generic(T msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            
-            
-                Maximum_Number obj1 = new Maximum_Number();
-                obj1.Find_Max_Num(25, 56, 88);
-            
+            Generic<string> gen = new Generic<string>("This is generic class");
+            Generic<int> genI = new Generic<int>(101);
+            Generic<char> getCh = new Generic<char>('I');
         }
     }
 }
